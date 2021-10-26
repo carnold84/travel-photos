c89edd30573bddaec36f4cfd9529549958b0a87a
carnold84 <carnold84@users.noreply.github.com>
2021-10-26 16:33:36 +1300

    Updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Location } from '@reach/router';
-import Routes from './Routes';
+import Routes from './routes';
 import StoreProvider from './store';
 
 const App = () => {
   return (
     <StoreProvider>
-      <Location>{({ location }) => <Routes />}</Location>
+      <Location>{({ location }) => <Routes location={location} />}</Location>
     </StoreProvider>
   );
 };
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,18 +1,17 @@
-import { Router, useLocation } from '@reach/router';
+import { useEffect } from 'react';
+import { Router } from '@reach/router';
 import { AnimatePresence } from 'framer-motion';
 
 import './routes.css';
-import { useFetchInitialData, useStore } from './hooks';
-import List from './views/List';
-import Collection from './views/Collection';
-import { useEffect } from 'react';
-import Photo from './views/Photo/Photo';
-import Upload from './views/Upload';
-import { useRoutesData } from './hooks/hooks';
+import { useFetchInitialData, useStore } from '../hooks';
+import List from '../views/List';
+import Collection from '../views/Collection';
+import Photo from '../views/Photo/Photo';
+import Upload from '../views/Upload';
+import { useRoutesData } from '../hooks/hooks';
 
-const Routes = () => {
+const Routes = ({ location }) => {
   const { state } = useStore();
-  const location = useLocation();
   const fetchInitialData = useFetchInitialData();
   const [routesData, setRoutesData] = useRoutesData();
 
@@ -38,7 +37,7 @@ const Routes = () => {
         <List exact={true} path={'/'} />
         <Upload exact={true} path={'/upload'} />
         <Collection exact={true} path={'/collection/:collectionId'} />
-        <Photo exact={true} path={'/collection/:collectionId/photo/:id'} />
+        <Photo exact={true} path={'/photo/:id'} />
       </Router>
     </AnimatePresence>
   );
