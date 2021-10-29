6d37b095949ea79ffc9c6bc725d58bf4f224edaa
carnold84 <carnold84@users.noreply.github.com>
2021-10-29 21:40:59 +1300

    Removed code splitting

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,16 +1,14 @@
-import { lazy, Suspense, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
 import './routes.css';
 import { useFetchInitialData, useStore } from '../hooks';
-import LoadingScreen from '../components/LoadingScreen';
+import List from '../views/List';
+import Collection from '../views/Collection';
+import Photo from '../views/Photo/Photo';
+import Upload from '../views/Upload';
 //import { useRoutesData } from '../hooks/hooks';
-
-const List = lazy(() => import('../views/List'));
-const Collection = lazy(() => import('../views/Collection'));
-const Photo = lazy(() => import('../views/Photo'));
-const Upload = lazy(() => import('../views/Upload'));
 
 const Routes = () => {
   const { state } = useStore();
@@ -32,22 +30,17 @@ const Routes = () => {
   }, []); */
 
   if (state.isLoading) {
-    return <LoadingScreen />;
+    return 'Loading...';
   }
 
   return (
     <AnimatePresence initial={false}>
-      <Suspense fallback={<LoadingScreen />}>
-        <Switch key={location.pathname} location={location}>
-          <Route
-            component={Photo}
-            path={'/collection/:collectionId/photo/:id'}
-          />
-          <Route component={Collection} path={'/collection/:collectionId'} />
-          <Route component={Upload} path={'/upload'} />
-          <Route component={List} path={'/'} />
-        </Switch>
-      </Suspense>
+      <Switch key={location.pathname} location={location}>
+        <Route component={Photo} path={'/collection/:collectionId/photo/:id'} />
+        <Route component={Collection} path={'/collection/:collectionId'} />
+        <Route component={Upload} path={'/upload'} />
+        <Route component={List} path={'/'} />
+      </Switch>
     </AnimatePresence>
   );
 };
